c67e6ff8c8c0da74192f430c4cbc53bca41f8b29
jledinh <jledinh@live.unc.edu>
2017-04-23 20:46:06 -0400

    Update DNF.java

--- DNF.java
+++ DNF.java
@@ -1,20 +1,30 @@
+package com.examples.cloud.speech;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class DNF extends JFrame { // implements Runnable{	
+public class dnf extends JFrame implements KeyListener{ // implements Runnable{	
 	private JPanel titlePanel, textPanel;
 	private JLabel titleLabel, textLabel;
 	private JTextField inputText;
-	
-	public DNF(){
+	private TemplateConverter t;
+	int position;
+	public dnf(TemplateConverter a){
+		t = a;
 		createPanel();
-		
 		setTitle("Diction N' Fiction");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		setResizable(false);
+		addKeyListener(this);
 		setSize(600,600);	
 		setVisible(true);
+		setFocusable(true);
+		position = 0;
+		
 	}
 	
 	private void createPanel() {
@@ -32,8 +42,7 @@ public class DNF extends JFrame { // implements Runnable{
 		textPanel.setBounds(0, 120, 600, 480);
 		textPanel.setBackground(new Color(0xe9c97e));
 		getContentPane().add(textPanel);
-		
-		textLabel = new JLabel("Please enter a noun:");
+		textLabel = new JLabel(t.getField().get(position));
 //			TODO: make "a noun" be replaceable with "an adj" or "a verb" or "an animal" etc.
 		textLabel.setPreferredSize(new Dimension(300, 450));
 		textLabel.setFont(new Font("Arial", Font.BOLD, 24));
@@ -55,11 +64,43 @@ public class DNF extends JFrame { // implements Runnable{
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-			@Override
 			public void run() {
-				new DNF();
+				TemplateConverter t = new TemplateConverter("/Users/jledinh/Documents/hello/src/main/java/file.txt");
+				new dnf(t);
+				
 			}
 		});			
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getKeyChar() == KeyEvent.VK_ENTER) {
+            System.out.println(position);
+            if (position != t.getField().size() - 1) {
+            	position += 1;
+            	textLabel.setText(t.getField().get(position));
+            	validate();
+            	repaint();
+            }
+            else {
+            	textLabel.setText(t.everything);
+            	validate();
+            	repaint();
+            }
+        }
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
