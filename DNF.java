59f5c0338cfa4605b2741e5ee2e70184ebae4210
annaxu3 <annajxu.3@gmail.com>
2017-04-25 21:31:31 -0400

    Update DNF.java
    reformatted gui

--- DNF.java
+++ DNF.java
@@ -1,4 +1,4 @@
-package com.examples.cloud.speech;
+package fsdf;
 import javax.swing.*;
 
 import org.apache.commons.cli.CommandLine;
@@ -27,8 +27,8 @@ import java.util.concurrent.Executors;
 
 
 public class dnf extends JFrame implements KeyListener{ // implements Runnable{	
-	private JPanel titlePanel, textPanel, checkPanel;
-	private JLabel titleLabel, textLabel, checkLabel;
+	private JPanel titlePanel, textPanel, checkPanel, storyPanel;
+	private JLabel titleLabel, textLabel, checkLabel, inputLabel;
 	private JTextField inputText,checkText;
 	private TemplateConverter t;
 	public static ArrayList<String> wordsList= new ArrayList<String>();
@@ -40,37 +40,61 @@ public class dnf extends JFrame implements KeyListener{ // implements Runnable{
 		setTitle("Diction N' Fiction");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
-		setResizable(false);
+		setResizable(true);
 		addKeyListener(this);
-		setSize(600,600);	
+		setSize(1440,800);	
 		setVisible(true);
 		setFocusable(true);
 		position = 0;
-		
+
 	}
-	
+
 	private void createPanel() {
+		getContentPane().setLayout(null);
+
 		titlePanel = new JPanel();
-		titlePanel.setBounds(0, 0, 600, 120);
+		titlePanel.setBounds(0, 0, 1440, 200);
 		titlePanel.setBackground(new Color(0xC07F7F));
 		getContentPane().add(titlePanel);
-		
-		titleLabel = new JLabel("DICTION N' FICTION");
-		titleLabel.setPreferredSize(new Dimension(500, 100));
-		titleLabel.setFont(new Font("Arial", Font.BOLD, 48));
+
+		titleLabel = new JLabel("DICTION N' FICTION", SwingConstants.CENTER);
+		titleLabel.setPreferredSize(new Dimension(800, 200));
+		titleLabel.setFont(new Font("Arial", Font.BOLD, 64));
 		titlePanel.add(titleLabel);
-			
+
+
 		textPanel = new JPanel();
-		textPanel.setBounds(0, 120, 600, 480);
+		textPanel.setBounds(0, 200, 1440, 620);
+		textPanel.setLayout(new GridLayout(3,1)); 
 		textPanel.setBackground(new Color(0xe9c97e));
-		getContentPane().add(textPanel);
-		textLabel = new JLabel(t.getField().get(position));
-//			TODO: make "a noun" be replaceable with "an adj" or "a verb" or "an animal" etc.
-		textLabel.setPreferredSize(new Dimension(300, 450));
-		textLabel.setFont(new Font("Arial", Font.BOLD, 24));
+		//		getContentPane().add(textPanel);
+
+		textLabel = new JLabel(t.getField().get(position), SwingConstants.CENTER); //SwingConstants.CENTER
+		textLabel.setPreferredSize(new Dimension(400, 800));
+		textLabel.setFont(new Font("Arial", Font.BOLD, 48));
+		//		textLabel.setHorizontalTextPosition(JLabel.CENTER);
+		//		textLabel.setVerticalTextPosition(JLabel.CENTER);
 		textPanel.add(textLabel);
-		
-/* 		checkPanel= new JPanel();
+
+		inputLabel = new JLabel();
+		inputLabel.setPreferredSize(new Dimension(400, 800));
+		inputLabel.setText("Batman");
+		inputLabel.setFont(new Font("Arial", Font.BOLD, 48));
+//		inputLabel.setHorizontalTextPosition(JLabel.CENTER);
+//		inputLabel.setVerticalTextPosition(JLabel.CENTER);
+		textPanel.add(inputLabel);
+
+//		inputText = new JTextField();
+//		//		TODO: make word spoken appear in the box
+//		inputText.setPreferredSize(new Dimension(250, 50));
+//		inputText.setText("Batman"); //insert input word...
+//		inputText.setFont(new Font("Arial", Font.BOLD, 24));
+//		textPanel.add(inputText);
+
+		getContentPane().add(textPanel);
+
+
+		/* 		checkPanel= new JPanel();
 		checkPanel.setBounds(0,601,600,480);
 		checkPanel.setBackground(new Color(0xffffff));
 		getContentPane().add(checkPanel);
@@ -78,157 +102,146 @@ public class dnf extends JFrame implements KeyListener{ // implements Runnable{
 		checkLabel.setPreferredSize(new Dimension(300, 450));
 		checkLabel.setFont(new Font("Arial", Font.BOLD, 24));
 		checkPanel.add(checkLabel); */
-		
-		
-		inputText = new JTextField();
-//			TODO: make word spoken appear in the box
-		inputText.setPreferredSize(new Dimension(250, 50));
-		inputText.setText("Batman"); //insert input word...
-		inputText.setFont(new Font("Arial", Font.BOLD, 24));
-		textPanel.add(inputText);		
-	
-//		SwingUtilities.updateComponentTreeUI(this);
-//		this.invalidate();
-//		this.validate();
-//		this.repaint();
-		
+
 	}
 
 	public static void main(final String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
-				TemplateConverter t = new TemplateConverter("/Users/jledinh/Documents/hello/src/main/java/file.txt");
-	
-					//speechRun(args);
-	
-				
+				TemplateConverter t = new TemplateConverter("/Users/annaxu/Documents/workspace/test/src/main/java/fsdf/file.txt");
+
+				//speechRun(args);
+
+
 				new dnf(t);
-				
-				
+
+
 			}
 		});			
 	}
-    public void clientSetup(){
-    	
-	    CommandLineParser parser = new DefaultParser();
-	    
-	    Options options = new Options();
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("host")
-	            .desc("endpoint for api, e.g. speech.googleapis.com")
-	            .hasArg()
-	            .argName("ENDPOINT")
-	            .build());
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("port")
-	            .desc("SSL port, usually 443")
-	            .hasArg()
-	            .argName("PORT")
-	            .build());
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("sampling")
-	            .desc("Sampling Rate, i.e. 16000")
-	            .hasArg()
-	            .argName("RATE")
-	            .build());
-	//    return options;
-    }
-    static ManagedChannel createChannel(String host, int port) throws IOException {
-    	  final List<String> OAUTH2_SCOPES =
-    		      Arrays.asList("https://www.googleapis.com/auth/cloud-platform");
-        GoogleCredentials creds = GoogleCredentials.getApplicationDefault();
-        creds = creds.createScoped(OAUTH2_SCOPES);
-        ManagedChannel channel =
-            ManagedChannelBuilder.forAddress(host, port)
-                .intercept(new ClientAuthInterceptor(creds, Executors.newSingleThreadExecutor()))
-                .build();
+	public void clientSetup(){
 
-        return channel;
-      }
-    public String speechRun(String[] args) throws InterruptedException, IOException{
-	    CommandLineParser parser = new DefaultParser();
-	    String host = null;
-	    Integer port = null;
-	    Integer sampling = null;
-	    Options options = new Options();
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("host")
-	            .desc("endpoint for api, e.g. speech.googleapis.com")
-	            .hasArg()
-	            .argName("ENDPOINT")
-	            .build());
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("port")
-	            .desc("SSL port, usually 443")
-	            .hasArg()
-	            .argName("PORT")
-	            .build());
-	    options.addOption(
-	        Option.builder()
-	            .longOpt("sampling")
-	            .desc("Sampling Rate, i.e. 16000")
-	            .hasArg()
-	            .argName("RATE")
-	            .build());
-        try {
-            CommandLine line = parser.parse(options, args);
-            
-            host = line.getOptionValue("host", "speech.googleapis.com");
-            port = Integer.parseInt(line.getOptionValue("port", "443"));
-          // System.out.println(port);
-            /*if (line.hasOption("sampling")) {
+		CommandLineParser parser = new DefaultParser();
+
+		Options options = new Options();
+		options.addOption(
+				Option.builder()
+				.longOpt("host")
+				.desc("endpoint for api, e.g. speech.googleapis.com")
+				.hasArg()
+				.argName("ENDPOINT")
+				.build());
+		options.addOption(
+				Option.builder()
+				.longOpt("port")
+				.desc("SSL port, usually 443")
+				.hasArg()
+				.argName("PORT")
+				.build());
+		options.addOption(
+				Option.builder()
+				.longOpt("sampling")
+				.desc("Sampling Rate, i.e. 16000")
+				.hasArg()
+				.argName("RATE")
+				.build());
+		//    return options;
+	}
+	static ManagedChannel createChannel(String host, int port) throws IOException {
+		final List<String> OAUTH2_SCOPES =
+				Arrays.asList("https://www.googleapis.com/auth/cloud-platform");
+		GoogleCredentials creds = GoogleCredentials.getApplicationDefault();
+		creds = creds.createScoped(OAUTH2_SCOPES);
+		ManagedChannel channel =
+				ManagedChannelBuilder.forAddress(host, port)
+				.intercept(new ClientAuthInterceptor(creds, Executors.newSingleThreadExecutor()))
+				.build();
+
+		return channel;
+	}
+	public String speechRun(String[] args) throws InterruptedException, IOException{
+		CommandLineParser parser = new DefaultParser();
+		String host = null;
+		Integer port = null;
+		Integer sampling = null;
+		Options options = new Options();
+		options.addOption(
+				Option.builder()
+				.longOpt("host")
+				.desc("endpoint for api, e.g. speech.googleapis.com")
+				.hasArg()
+				.argName("ENDPOINT")
+				.build());
+		options.addOption(
+				Option.builder()
+				.longOpt("port")
+				.desc("SSL port, usually 443")
+				.hasArg()
+				.argName("PORT")
+				.build());
+		options.addOption(
+				Option.builder()
+				.longOpt("sampling")
+				.desc("Sampling Rate, i.e. 16000")
+				.hasArg()
+				.argName("RATE")
+				.build());
+		try {
+			CommandLine line = parser.parse(options, args);
+
+			host = line.getOptionValue("host", "speech.googleapis.com");
+			port = Integer.parseInt(line.getOptionValue("port", "443"));
+			// System.out.println(port);
+			/*if (line.hasOption("sampling")) {
               sampling = Integer.parseInt(line.getOptionValue("sampling"));
             } else {
               System.err.println("An Audio sampling rate (--sampling) must be specified. (e.g. 16000)");
               System.exit(1);
             }
-            */
-           // Avoid the command line thing
-           sampling = 16000;
-          } catch (ParseException exp) {
-            System.err.println("Unexpected exception:" + exp.getMessage());
-            System.exit(1);
-          }
-         
-          
-          ManagedChannel channel = createChannel(host, port);
-          speech_to_text client = new speech_to_text(channel, sampling);
-          try {
-          System.out.println("reach");
-            client.recognize();
-            System.out.println("reach12");
-         wordsList = client.wordsList; 
-         return client.wordsList.get(i);
-       
-          } finally {
-          	client.shutdown();
-          	 i++;
-          	}
-          }
-    	
-    	
+			 */
+			// Avoid the command line thing
+			sampling = 16000;
+		} catch (ParseException exp) {
+			System.err.println("Unexpected exception:" + exp.getMessage());
+			System.exit(1);
+		}
+
+
+		ManagedChannel channel = createChannel(host, port);
+		speech_to_text client = new speech_to_text(channel, sampling);
+		try {
+			System.out.println("reach");
+			client.recognize();
+			System.out.println("reach12");
+			wordsList = client.wordsList;
+			//System.out.println(client.wordsList.get());
+			return client.wordsList.get(i);
+
+		} finally {
+			client.shutdown();
+			i++;
+		}
+	}
+
+
 
 
 	public void keyTyped(KeyEvent e) {
 		// TODO Auto-generated method stub
 		String args[]= null;
 		if (e.getKeyChar() == KeyEvent.VK_ENTER) {
-            //System.out.println(position);
-            if (position != t.getField().size()) {
-            	position += 1;
-            	if (position != 6) {
-            		textLabel.setText(t.getField().get(position));
-            	}
-            	//validate();
-            	//repaint();
-            	try {
-					inputText.setText(speechRun(args));
-					
+			//System.out.println(position);
+			if (position != t.getField().size()) {
+				position += 1;
+				if (position != 6) {
+					textLabel.setText(t.getField().get(position));
+				}
+				//validate();
+				//repaint();
+				try {
+					System.out.println(args);
+					inputLabel.setText(speechRun(args));
+
 				} catch (InterruptedException e1) {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
@@ -236,29 +249,44 @@ public class dnf extends JFrame implements KeyListener{ // implements Runnable{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-             	validate();
-            	repaint();
-            }
-            else {
-            	for (int i=0;i<wordsList.size();i++) {
-            		System.out.println(wordsList.get(i));
-            	}
-            	t.InsertField(wordsList);
-            	textLabel.setText(t.everything);
-            	textLabel.setFont(new Font("Arial", Font.PLAIN, 12));
-            	validate();
-            	repaint();
-            }
-        }
-		//System.out.println(e.getID());
-	//	System.out.println("hi");
-		if (e.getKeyChar() == KeyEvent.VK_A) {
-           // System.out.println(position);
-     //       if (position != t.getField().size() - 1) {
-      //      	position += 1;
-                System.out.println("hi");
-/*            	try {
+				validate();
+				repaint();
+			}
+			else {
+				for (int i=0;i<wordsList.size();i++) {
+					System.out.println(wordsList.get(i));
+				}
+				t.InsertField(wordsList);
+				textLabel.setText(t.everything);
+				textLabel.setFont(new Font("Arial", Font.PLAIN, 12));
 				
+//				storyPanel = new JPanel();
+				
+				
+				getContentPane().remove(textPanel);
+				
+				getContentPane().add(storyPanel);
+				revalidate();
+				repaint();
+				
+				
+/* 
+ * frame.getContentPane().remove() or removeAll();
+ * frame.getContentPane().add(allTheNewComponents);
+ * frame.revalidate();
+ * frame.repaint();
+ * */
+			}
+		}
+		//System.out.println(e.getID());
+		//	System.out.println("hi");
+		if (e.getKeyChar() == KeyEvent.VK_A) {
+			// System.out.println(position);
+			//       if (position != t.getField().size() - 1) {
+			//      	position += 1;
+			System.out.println("hi");
+			/*            	try {
+
 	            	validate();
 	            	repaint();
 				} catch (InterruptedException e1) {
@@ -268,28 +296,28 @@ public class dnf extends JFrame implements KeyListener{ // implements Runnable{
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}*/
-       //     	validate();
-       //     	repaint();
-        //    }
-        //    else {
-         //   	textLabel.setText(t.everything);
-          //  	validate();
-          //  	repaint();
-            }
-        }
-	
+			//     	validate();
+			//     	repaint();
+			//    }
+			//    else {
+			//   	textLabel.setText(t.everything);
+			//  	validate();
+			//  	repaint();
+		}
+	}
+
 
 
 
 	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
