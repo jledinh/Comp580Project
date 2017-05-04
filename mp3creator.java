24a895ed8462f19c2b919f9b22c7e17e26b8fd4e
Eric Zhou <irishswedens@gmail.com>
2017-05-04 15:16:43 -0400

    Update mp3creator.java

--- mp3creator.java
+++ mp3creator.java
@@ -4,7 +4,7 @@ public class mp3creator {
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		TextToMp3 ttm = new TextToMp3("Please hit enter and then say the same animal.", "/Users/jledinh/Documents/dnf_mp3/animal_same.mp3");
+		TextToMp3 ttm = new TextToMp3("Please hit enter and then say the same animal.", "/Users/-------/Documents/dnf_mp3/animal_same.mp3");
 	}
 
 }
