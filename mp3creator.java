78dd27dcb53dadb9619e92890740b8f53059b392
jledinh <jledinh@live.unc.edu>
2017-05-04 14:48:07 -0400

    Create mp3creator.java
    Used to create the mp3 files for Text to Speech

---
+++ mp3creator.java
@@ -0,0 +1,10 @@
+package com.examples.cloud.speech;
+
+public class mp3creator {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		TextToMp3 ttm = new TextToMp3("Please hit enter and then say the same animal.", "/Users/jledinh/Documents/dnf_mp3/animal_same.mp3");
+	}
+
+}
